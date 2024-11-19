de03c2919c47ffd5ca9a0cb46ebcff6713a5f8c8
eutagocode <thiagoluix21@gmail.com>
2024-11-19 20:50:04 -0300

    feat: :sparkles: Estudando rotas no express sobre params, body e query

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 import express from "express";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
-const consoleBody = (request, response, next) => {
-    console.log(request.body);
-    next();
-};
+let alunos = [
+    { id: 0, name: "Jose" },
+    { id: 1, name: "Maria" },
+    { id: 2, name: "Joao" },
+    { id: 3, name: "Marcos" },
+];
 
-const hello = (request, response) => {
-    response.send("hello world");
-};
+app.use(express.urlencoded());
 
-app.use("/", express.json());
-app.use("/", consoleBody);
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
 
-app.get("/", hello);
-app.post("/", hello);
+app.get("/alunos", (req, res) => {
+    res.json(JSON.stringify(alunos));
+});
+
+// Body
+app.get("/aluno", (req, res) => {
+    let aluno = alunos.filter((aluno) => aluno.id == req.body.id);
+    res.json(aluno);
+});
+
+// Params
+app.get("/aluno/:id", (req, res) => {
+    let aluno = alunos.filter((aluno) => aluno.id == req.params.id);
+    console.log(req.params.id);
+    res.json(aluno);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
